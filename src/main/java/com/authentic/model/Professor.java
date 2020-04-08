1df1dd7aeb3e5704ab21ef53c80b662be948afd2
Vinícius Gomes Ferreira <neogedom@gmail.com>
2020-04-08 13:44:23 -0400

    Creating Requesting and Professor class

--- src/main/java/com/authentic/model/Professor.java
+++ src/main/java/com/authentic/model/Professor.java
@@ -1,11 +1,10 @@
 package com.authentic.model;
 
 //Por enquanto, a única razão de termos essa classe é para marcar um perfil
-public class Student extends Person {
+public class Professor extends Person {
 
     //Atribuindo um perfil no ato de construção do objeto
-    public Student(String nome, String username, String password) {
-        super(nome, username, password, Student.class);
+    public Professor(String nome, String username, String password) {
+        super(nome, username, password, Professor.class);
     }
-
 }
--- src/main/java/com/authentic/model/Requesting.java
+++ src/main/java/com/authentic/model/Requesting.java
@@ -1,11 +1,10 @@
 package com.authentic.model;
 
 //Por enquanto, a única razão de termos essa classe é para marcar um perfil
-public class Student extends Person {
+public class Requesting extends Person {
 
     //Atribuindo um perfil no ato de construção do objeto
-    public Student(String nome, String username, String password) {
-        super(nome, username, password, Student.class);
+    public Requesting(String nome, String username, String password) {
+        super(nome, username, password, Requesting.class);
     }
-
 }
